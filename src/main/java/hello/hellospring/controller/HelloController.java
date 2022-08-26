5a8fa0311e0b5c2a9972bc3077a2a529e5d46988
SHIM <weasd33@naver.com>
2022-08-27 00:10:30 +0900

    View 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello") // 웹 어플리케이션에서 /hello라고 들어오면 해당 메소드를 실행시켜준다.
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); // model에 (키, 값)을 담아서
+        return "hello"; // templates/hello.html을 찾아간다.
+    }
+}
