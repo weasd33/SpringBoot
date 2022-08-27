e5574fd17e3a51630d483908da3e3e7ca6b7f90a
SHIM <weasd33@naver.com>
2022-08-27 15:27:54 +0900

    API - @ResponseBody의 문자와 객체 반환

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // StringConverter 동작 -> 문자 반환
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring!!"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // JsonConverter 동작 -> JSON 반환
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // {"name":"spring!!"}
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
